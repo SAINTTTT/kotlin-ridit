9ed25d7b44503c356ebc54c262a782694f36a659
cl-fiuba <cleith@fi.uba.ar>
2024-05-31 01:58:41 -0300

    Se puede crear un post. El usuario no es dinámico. Falta formatear la fecha de creación

--- app/src/main/java/com/example/kotlin_ridit/CreatePostActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/CreatePostActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class CreatePostActivity : AppCompatActivity() {
 
@@ -33,6 +40,38 @@ class CreatePostActivity : AppCompatActivity() {
             val title = etPostTitle.text.toString()
             val content = etPostContent.text.toString()
             if (title != "" && content != "") {
+                val db = Firebase.firestore
+                db.collection("posts").document()
+                    .set(
+                        hashMapOf(
+                            "creator" to "juancito",
+                            "title" to title,
+                            "content" to content,
+                            "upvoteCount" to 0,
+                            "downvoteCount" to 0,
+                            "commentsCount" to 0,
+                            "creationDate" to java.util.Date()
+                        )
+                    )
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            applicationContext,
+                            getString(R.string.post_creation_success), Toast.LENGTH_SHORT
+                        ).show();
+                        etPostTitle.text.clear()
+                        etPostContent.text.clear()
+                        Log.d(
+                            "FIRESTORE-CREATE-POST",
+                            "DocumentSnapshot successfully written!"
+                        )
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(
+                            "FIRESTORE-CREATE-POST",
+                            "Error writing document",
+                            e
+                        )
+                    }
                 Log.i("POST", "$title, $content")
             }
         }
