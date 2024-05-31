7ed247ac950eceacab63ba3156e59d892f389fa2
cl-fiuba <cleith@fi.uba.ar>
2024-05-31 02:25:44 -0300

    Se puede crear un comentario. El usuario no es dinámico. El id del post no es dinámico

--- app/src/main/java/com/example/kotlin_ridit/CreateCommentActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/CreateCommentActivity.kt
@@ -5,10 +5,10 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CreateCommentActivity : AppCompatActivity() {
 
@@ -36,8 +36,38 @@ class CreateCommentActivity : AppCompatActivity() {
             val comment = etCommentContent.text.toString()
 
             if (comment != "") {
-                Log.i("COMMENT", "Comentario: $comment")
+                val db = Firebase.firestore
+                db.collection("comments").document()
+                    .set(
+                        hashMapOf(
+                            "commentsTo" to "/posts/zwS5wGVCfE6y1TKRZHxu",
+                            "content" to comment,
+                            "creator" to "usuario X",
+                            "creationDate" to java.util.Date()
+                        )
+                    )
+                    .addOnSuccessListener {
+                        Toast.makeText(
+                            applicationContext,
+                            getString(R.string.comment_created), Toast.LENGTH_SHORT
+                        ).show();
+                        etCommentContent.text.clear()
+
+                        Log.d(
+                            "FIRESTORE-CREATE-COMMENT",
+                            "DocumentSnapshot successfully written!"
+                        )
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(
+                            "FIRESTORE-CREATE-COMMENT",
+                            "Error writing document",
+                            e
+                        )
+                    }
+                Log.i("POST", "comentario: $comment")
             }
         }
     }
 }
+
