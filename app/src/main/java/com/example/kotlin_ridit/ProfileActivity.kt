4f0bbfceebcd4e86940737ce063820f36f6ff04d
cl-fiuba <cleith@fi.uba.ar>
2024-06-01 03:16:04 -0300

    Se crea pantalla para datos personales. Se puede navegar hacia ella desde el perfil

--- app/src/main/java/com/example/kotlin_ridit/ProfileActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/ProfileActivity.kt
@@ -1,14 +1,30 @@
 package com.example.kotlin_ridit
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var tvPersonalData: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        initComponents()
+        initUI()
     }
+
+    private fun initComponents() {
+        tvPersonalData = findViewById(R.id.tvPersonalData)
+    }
+
+    private fun initUI() {
+        tvPersonalData.setOnClickListener { navigateToPersonalData() }
+    }
+
+    private fun navigateToPersonalData() {
+        val intent = Intent(this, PersonalDataActivity::class.java)
+        startActivity(intent)
+    }
+
 }
