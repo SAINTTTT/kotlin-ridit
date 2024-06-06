3174016f441fa1602d7fc6b28e7a96f3043c0ffd
cl-fiuba <cleith@fi.uba.ar>
2024-06-06 02:45:12 -0300

    Un usuario logueado puede crear una comunidad

--- app/src/main/java/com/example/kotlin_ridit/CreateCommunityActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/CreateCommunityActivity.kt
@@ -1,10 +1,14 @@
 package com.example.kotlin_ridit
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CreateCommunityActivity : AppCompatActivity() {
     private lateinit var etCommunityName: EditText
@@ -31,14 +35,41 @@ class CreateCommunityActivity : AppCompatActivity() {
     private fun createCommunity() {
         val communityName = etCommunityName.text.toString()
         val communityDescription = etCommunityDescription.text.toString()
+        val db = Firebase.firestore
+        val currentUser = Firebase.auth.currentUser
 
         if (communityName.isNotEmpty() && communityDescription.isNotEmpty()) {
             //TODO hay que validar que ese nombre no exista
-            Toast.makeText(
-                baseContext, "Comunidad Creada",
-                Toast.LENGTH_SHORT
-            ).show()
 
+            db.collection("communities").document()
+                .set(
+                    hashMapOf(
+                        "creator" to currentUser?.email,
+                        "description" to communityDescription,
+                        "name" to communityName,
+                        "members" to currentUser?.email, // cambiar esto
+                        "posts" to null, // cambiar esto
+                        "creationDate" to java.util.Date()
+                    )
+                )
+                .addOnSuccessListener {
+                    Toast.makeText(
+                        baseContext, "Comunidad Creada",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    Log.d(
+                        "FIRESTORE-CREATE-COMMUNITY",
+                        "DocumentSnapshot successfully written!"
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.w(
+                        "FIRESTORE-CREATE-COMMUNITY",
+                        "Error writing document",
+                        e
+                    )
+                }
             finish()
         }
     }
