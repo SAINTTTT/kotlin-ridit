43a2575ea914791904dc25f1d2ff36224e39970c
saintttt <alivetheoldschool@hotmail.com>
2024-06-07 14:10:43 -0300

    Agreagado de logica para obtener la data desde la base de datos para cada usuario

--- app/src/main/java/com/example/kotlin_ridit/PersonalDataActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/PersonalDataActivity.kt
@@ -1,12 +1,45 @@
 package com.example.kotlin_ridit
 
 import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class PersonalDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+        val db = Firebase.firestore
 
+        // Obtener referencias a los elementos de la interfaz
+        val tvUsername: TextView = findViewById(R.id.tvUsername)
+        val tvEmail: TextView = findViewById(R.id.tvEmail)
+        val btnChangeEmail: Button = findViewById(R.id.btnChangeEmail)
+        val userId = "miguela@google.com"
+
+        val docRef= db.collection("users").document(userId)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    tvUsername.text = document.data?.get("username").toString()
+                    tvEmail.text = document.id
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+
+
+        // Configurar el botón para cambiar el email
+//        btnChangeEmail.setOnClickListener {
+//            // Chequear cual es la mejor forma de cambiar el mail actual de un usuario
+//            val intent = Intent(this, ChangeEmailActivity::class.java)
+//            startActivity(intent)
+//        }
     }
+
+
 }
