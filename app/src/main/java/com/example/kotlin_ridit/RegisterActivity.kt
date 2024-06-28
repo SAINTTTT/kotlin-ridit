51c87b5f0270f3670b98788ee23a81c72577adcf
cl-fiuba <cleith@fi.uba.ar>
2024-06-28 04:55:32 -0300

    Mejoras estéticas en pantalla de registro

--- app/src/main/java/com/example/kotlin_ridit/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlin_ridit/RegisterActivity.kt
@@ -24,6 +24,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
         initComponents()
         initUI()
     }
